a7d9b66ad2af947116bc375dab3cc66ace94d343
vissnupriyasae <vissnupriyasae@gmail.com>
2017-06-13 19:39:10 -0500

    code indendation

--- src/utility/Constant.java
+++ src/utility/Constant.java
@@ -2,7 +2,6 @@ package utility;
 
 public class Constant {
 	public static final String URL = "<Eg: facebook URL>";
-<<<<<<< HEAD
 
 	public static final String Username = "<username>";
 
@@ -10,13 +9,4 @@ public class Constant {
 
 	public static final String Path_TestData = "<path to excel sheet>";
 
-=======
-	 
-    public static final String Username = "<username>";
-
-    public static final String Password = "<password>";
-
-   public static final String Path_TestData = "<path to excel sheet>";
-   
->>>>>>> e645053d56796db0d51cad098f3d0713995ded13
 }
--- src/utility/ExcelUtils.java
+++ src/utility/ExcelUtils.java
@@ -1,19 +1,12 @@
 package utility;
-<<<<<<< HEAD
 
 import java.io.FileInputStream;
 
 import java.io.FileOutputStream;
-=======
-import java.io.FileInputStream;
-	 
-    import java.io.FileOutputStream;
->>>>>>> e645053d56796db0d51cad098f3d0713995ded13
 
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 
-<<<<<<< HEAD
 import org.apache.poi.xssf.usermodel.XSSFRow;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -24,40 +17,19 @@ public class ExcelUtils {
 
 	private static XSSFSheet ExcelWSheet;
 
-=======
-	import org.apache.poi.xssf.usermodel.XSSFRow;
-
-	import org.apache.poi.xssf.usermodel.XSSFSheet;
-
-	import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-	
-public class ExcelUtils {
-	
-
-	private static XSSFSheet ExcelWSheet;
-	 
->>>>>>> e645053d56796db0d51cad098f3d0713995ded13
 	private static XSSFWorkbook ExcelWBook;
 
 	private static XSSFCell Cell;
 
 	private static XSSFRow Row;
-<<<<<<< HEAD
 
 	public static void setExcelFile(String Path, String SheetName) throws Exception {
 
 		try {
 
 			// Open the Excel file
-=======
-	public static void setExcelFile(String Path,String SheetName) throws Exception {
-		 
-			try {
 
-   			// Open the Excel file
->>>>>>> e645053d56796db0d51cad098f3d0713995ded13
-
-			FileInputStream ExcelFile = new FileInputStream("/Users/Documents/priya/TestData.xlsx");
+			FileInputStream ExcelFile = new FileInputStream("/Users/Documents/devops_priya/TestData.xlsx");
 
 			// Access the required test data sheet
 
@@ -65,7 +37,6 @@ public class ExcelUtils {
 
 			ExcelWSheet = ExcelWBook.getSheet("Sheet1");
 
-<<<<<<< HEAD
 		} catch (Exception e) {
 
 			throw (e);
@@ -126,68 +97,3 @@ public class ExcelUtils {
 
 	}
 }
-=======
-			} catch (Exception e){
-
-				throw (e);
-			}
-
-			}
-	
-			
-	       			public static void setCellData(String Result,int RowNum,int ColNum) throws Exception{
-	       			 
-	           			try{
-	     
-	              			Row  = ExcelWSheet.getRow(RowNum);
-	     
-	    					Cell = Row.getCell(ColNum, Row.RETURN_BLANK_AS_NULL);
-	     
-	    					if (Cell == null) {
-	     
-	    						Cell = Row.createCell(ColNum);
-	     
-	    						Cell.setCellValue(Result);
-	     
-	    						} else {
-	     
-	    							Cell.setCellValue(Result);
-	     
-	    						}
-	    					FileOutputStream fileOut = new FileOutputStream(Constant.Path_TestData);
-	    					 
-	          				ExcelWBook.write(fileOut);
-	 
-	          				fileOut.flush();
-	 
-	 						fileOut.close();
-	 
-							}catch(Exception e){
-	 
-								throw (e);
-	 
-						}
-	 
-					}
-	 
-
-	
-	public static String getCellData(int RowNum, int ColNum) throws Exception{
-		// TODO Auto-generated method stub
-		try{
-			 
-  			Cell = ExcelWSheet.getRow(RowNum).getCell(ColNum);
-
-  			String CellData = Cell.getStringCellValue();
-
-  			return CellData;
-
-  			}catch (Exception e){
-
-				return"";
-
-	}
-
-	}
-	}
->>>>>>> e645053d56796db0d51cad098f3d0713995ded13
